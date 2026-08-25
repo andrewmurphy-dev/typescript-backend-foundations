4f61b10d38aa7ef0c1cca6b32b3fa1f82dc7594a
Andrew Murphy <legionastartesofficial@gmail.com>
2026-08-26 02:43:33 +0900

    day 1, week 1

---
+++ week 1/topic_2.ts
@@ -0,0 +1,39 @@
+// what is const ?
+
+
+//const is used to create a variable whose value should not be reassigned later.
+
+
+//for example 
+
+const name = "Andy";
+
+console.log(name);
+
+//to run the file
+//  node topic_2.ts
+
+
+//questions 
+
+//question 1 
+
+
+const country = "Japan";
+
+console.log(country);
+
+//node topic_2.ts
+
+
+
+
+//question 2 
+
+const game = "World of Warcraft";
+
+console.log(game);
+
+
+//question 3
+
---
+++ week 1/topic_3.ts
@@ -0,0 +1,43 @@
+//let 
+
+
+//this is used to create a variable whose value can be reassigned later 
+
+//const → value should not be reassigned
+//let   → value can be reassigned
+
+
+//for example
+
+
+let score = 10;
+
+score = 20;
+
+console.log(score);
+
+
+//questions
+
+//question 1 
+
+let health = 100;
+
+health = 75;
+
+console.log(health);
+
+
+
+
+
+//question 2 
+
+let gold = 50;
+
+gold = 120;
+
+console.log(gold)
+
+
+
