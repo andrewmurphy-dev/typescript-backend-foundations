ce008f7795a7f8060cfbfeaecc9b3113c39c5058
Andrew Murphy <legionastartesofficial@gmail.com>
2026-08-27 03:59:20 +0900

    chore: configure TypeScript build output

---
+++ week 1/day 2/topic_1.ts
@@ -0,0 +1,24 @@
+//arithmetic operators 
+
+
+//+   // add
+//-   // subtract
+//*   // multiply
+//, /   // divide
+//%   // remainder
+
+
+//for example 
+
+const damage: number = 40;
+const bonus: number = 15;
+
+//question 1
+
+let baseDamage: number = 125;
+let criticalBonus: number = 75;
+
+let Total_damage = baseDamage + criticalBonus 
+
+console.log("Total damage: ", Total_damage)
+
